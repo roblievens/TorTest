2de2633cdb0057ac5202fee197f2fcfa6f04a047
roblievens <mail@roblievens.be>
2015-09-12 16:07:40 +0200

    Start

---
+++ Test ASP.net MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_ASP.net_MVC.Startup))]
+namespace Test_ASP.net_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
